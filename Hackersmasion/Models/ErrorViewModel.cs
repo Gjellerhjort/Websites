b5be1421c7dad6c2069aa4b9e6a9f237288fc498
gjellerhjort <mads.gjellerod@gmail.com>
2022-05-23 14:13:47 +0200

    made a new asp.net core 6 MVC project

---
+++ Hackersmasion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Hackersmansion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
